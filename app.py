fdb2db68bc9d29c9c51ef18e7832a90e74035d21
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-12-02 16:54:33 -0500

    Database Setup

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+###############################################
 # Import Dependencies
+###############################################
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
+
+###############################################
+# Database Setup
+###############################################
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+mongo = PyMongo(app)
+
