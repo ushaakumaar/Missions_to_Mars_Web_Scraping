2055033c82f60c10567c6af7a3cb7112c38f4e5d
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-12-02 16:55:03 -0500

    Flask Setup & Routes

--- app.py
+++ app.py
@@ -13,3 +13,24 @@ import scrape_mars
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
 mongo = PyMongo(app)
 
+###############################################
+# Flask Setup & Routes
+###############################################
+
+# create an app
+app = Flask(__name__)
+
+#define endpoints
+@app.route("/")
+def index():
+    mars_facts = mongo.db.mars_facts.find_one()
+    return render_template("index.html", mars_facts=mars_facts)
+
+
+@app.route("/scrape")
+def scraper():
+    mars_facts = mongo.db.mars_facts
+    scrape_mars_list = scrape_mars.scrape()
+    mars_facts.update({}, scrape_mars_list, upsert=True)
+    return redirect("/", code=302)
+
